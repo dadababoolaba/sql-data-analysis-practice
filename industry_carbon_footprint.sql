3eccb729ae0a70d5afeed90224fa59063d0a09fb
dadababoolaba <evechen0115@gmail.com>
2025-12-23 22:34:06 +1300

    Update industry_carbon_footprint.sql

--- industry_carbon_footprint.sql
+++ industry_carbon_footprint.sql
@@ -1,1 +1,10 @@
+-- Industry-level carbon footprint analysis for the most recent year
 
+SELECT
+  industry_group,
+  COUNT(DISTINCT company) AS num_companies,
+  ROUND(SUM(carbon_footprint_pcf), 1) AS total_industry_footprint
+FROM product_emissions
+WHERE year = (SELECT MAX(year) FROM product_emissions)
+GROUP BY industry_group
+ORDER BY total_industry_footprint DESC;
